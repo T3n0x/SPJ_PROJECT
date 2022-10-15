2610b26f333e0cc777247cc693122a2338acd0b1
T3n0x <paulogutierrez211@gmail.com>
2022-10-15 20:26:17 -0300

    update modals

--- src/Components/Navar.js
+++ src/Components/Navar.js
@@ -6,16 +6,25 @@ class Navar extends Component {
     constructor(props) {
       super(props);
   
-      this.state = { isOpen: false };
+      this.state = { contact_isOpen: false, login_isOpenisOpen: false };
     }
 
     toggle_modal_contact = () => {
         this.setState({
-            isOpen: !this.state.isOpen
+            contact_isOpen: !this.state.contact_isOpen
         });
-      
+    }
+
+    toggle_modal_login = () => {
+        this.setState({
+            login_isOpen: !this.state.login_isOpen
+        });
     }
     
+    formulario = () => {
+        alert('Esta wea no hace nada aun')
+    }
+
     render() {
         return(
             <nav>
@@ -24,27 +33,23 @@ class Navar extends Component {
                 </div>
                 <div className='btn_items'>
                     <li>
-                        <button onClick={this.toggle_modal_contact}>
-                            Contactanos
-                        </button>
+                        <button onClick={this.toggle_modal_contact}>Contactanos</button>
                     </li>
     
                     <li>
-                        <button>
-                            Iniciar Sesion
-                        </button>
+                        <button onClick={this.toggle_modal_login}>Iniciar Sesion</button>
                     </li>
                 </div>
 
                 {/* Modal de contacto */}
-                <Modal show={this.state.isOpen}
-                    onClose={this.toggle_modal_contact} title='Contactanos'>
+                <Modal show={this.state.contact_isOpen}
+                    onClose={this.toggle_modal_contact}>
                     
                     <div className='contact'>
                         <h1>Contactanos</h1>
                         <p>Puedes contactarnos a travez de <a href='/'>Whatsapp</a> o nuestro <a href='mailto:'>Email</a>, 
                         tambien puedes completar el siguiente formulario y nos pondremos en contacto a la brefdedad.</p>
-                        <form action='' method='' style={{marginTop:12}}>
+                        <form onSubmit={this.formulario} method='' style={{marginTop:12}}>
                             <div style={{display:'grid', gridTemplateColumns: '1fr 1fr', gridGap:20}}>
                                 <label>Nombre</label>
                                 <label>Numero Telefono</label>
@@ -61,7 +66,15 @@ class Navar extends Component {
                             <button className='btn_lx blue'>Enviar Formulario</button>
                         </form>
                     </div>
+                </Modal>
+
+
+                <Modal show={this.state.login_isOpen}
+                    onClose={this.toggle_modal_login}>
                     
+                    <div className='contact'>
+                        <h1>Aca va la wea de inicio de sesion</h1>
+                    </div>
                 </Modal>
             </nav>
         );
